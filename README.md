3763fab66466e179f50f9f923d84dd1c2032ced8
Louis Lafosse <114692238+louislafosse@users.noreply.github.com>
2023-09-03 23:36:16 +0200

    feat(impl) : Implementation of the lib

---
+++ README.md
@@ -0,0 +1,38 @@
+# lib-load
+A litle lib binding around LoadLibraryA and GetProcAddress from winapi crate in order to hide imports functions from winapi
+
+## Usage
+*Refer to main.rs for more documentation about Error handling.*
+
+dll:
+```rs
+#[no_mangle]
+pub extern "C" fn hello_from_dll() {
+    println!("Hello from Rust DLL!");
+}
+```
+
+main:
+```rs
+mod loader;
+use loader::LoadMod;
+
+{
+    let dll = LoadMod::new("my_dll/target/release/my_dll.dll").unwrap();
+
+    // Use this proc macro and all the work around will be handled
+    let result = call_function!(&dll_handle, fn() -> i32, "hello_from_dll!");
+
+    result();
+    // Memory will be automatically freed
+}
+```
+
+output
+```bash
+lib-load> cargo run --release
+    Finished release [optimized] target(s) in 0.00s
+     Running `target\release\loader.exe`
+
+Hello from Rust DLL!
+```
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod loader;
+use loader::LoadMod;
+
+use std::os::raw::c_int;
+
+fn main() {
+    
+    // Load the DLL dynamically
+    let dll_handle = LoadMod::new("my_dll/target/release/my_dll.dll");
+    if dll_handle.is_err() {
+        println!("Failed to load DLL: {}", dll_handle.err().unwrap());
+        return;
+    }
+    let dll_handle = dll_handle.unwrap();
+    // Load the function pointer dynamically
+    // Convert the function pointer to the defined function type
+    let result = call_function!(&dll_handle, fn() -> i32, "hello_from_dll");
+
+    let out = result();
+    // Now, you can call the hello_from_dll function
+    println!("Function result: {}", out);
+
+    let add_function = call_function!(&dll_handle, fn(c_int, c_int) -> c_int, "add");
+    let result = add_function(1, 2);
+    println!("Function result: {}", result);
+
+    // The DLL will be freed when the Handle goes out of scope
+}
